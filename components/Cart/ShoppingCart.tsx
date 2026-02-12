5ff9befc96cc508c4cadbb093018aee022d2a36e
SRT DEV <sahirruiz4@gmail.com>
2026-02-11 23:45:54 -0500

    feat: Create a ShoppingCart component to display the sales summary

---
+++ components/Cart/ShoppingCart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ShoppingCart() {
+  return (
+    <>
+    <h2 className='text-4xl font-bold text-gray-900'> Resumen de Venta</h2>
+    
+    
+    </>
+  )
+}
