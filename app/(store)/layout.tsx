849303568397f9c86e214db0aef744a7830acfbd
SRT DEV <sahirruiz4@gmail.com>
2026-02-11 23:45:41 -0500

    feat: Integrate ShoppingCart component into the main design

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,3 +1,4 @@
+import ShoppingCart from "@/components/Cart/ShoppingCart";
 import MainNav from "@/components/ui/MainNav";
 
 
@@ -14,7 +15,9 @@ export default function RootLayout({
                     {children}
                 </div>
                 <aside className="md:w-96 md:h-screen md:overflow-y-scroll pt-10 pb-32 px-5 ">
-                   
+                   <ShoppingCart>
+                    
+                   </ShoppingCart>
                 </aside>
             </main>
         </>
