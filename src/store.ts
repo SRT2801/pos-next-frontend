754216ae39f1c75d11e9b3416d4bb10bf104e2e4
SRT DEV <sahirruiz4@gmail.com>
2026-02-11 23:46:24 -0500

    feat: Implement Zustand store for managing shopping cart state

---
+++ src/store.ts
@@ -0,0 +1,55 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+import { Product, ShoppingCart } from "./schemas";
+
+interface Store {
+  total: number;
+  contents: ShoppingCart;
+
+  addToCart: (product: Product) => void;
+}
+
+export const useStore = create<Store>()(
+  devtools((set, get) => ({
+    total: 0,
+    contents: [],
+    addToCart: (product: Product) => {
+      const { id: productId, categoryId, ...data } = product;
+      console.log(product);
+      let contents: ShoppingCart = [];
+      const duplicated = get().contents.findIndex(
+        (item) => item.productId === productId,
+      );
+
+      if (duplicated >= 0) {
+        if (
+          get().contents[duplicated].quantity >=
+          get().contents[duplicated].inventory
+        )
+          return;
+
+        contents = get().contents.map((item) =>
+          item.productId === productId
+            ? {
+                ...item,
+                quantity: item.quantity + 1,
+              }
+            : item,
+        );
+      } else {
+        contents = [
+          ...get().contents,
+          {
+            ...data,
+            quantity: 1,
+            productId,
+          },
+        ];
+      }
+
+      set(() => ({
+        contents,
+      }));
+    },
+  })),
+);
