3867f90567ea3fed2ef5d279d2f9f2ab3541320d
SRT DEV <sahirruiz4@gmail.com>
2026-02-11 22:54:18 -0500

    feat: Add formatCurrency utility function for currency formatting

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat("es-CO", {
+    style: "currency",
+    currency: "COP",
+    minimumFractionDigits: 3,
+  }).format(amount);
+}
