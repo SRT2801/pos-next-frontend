6f8ea47d070a1e604458c77959b0b53070bfd723
SRT DEV <sahirruiz4@gmail.com>
2026-02-11 21:07:21 -0500

    feat: Add image configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+  
+    unoptimized: process.env.NODE_ENV === "development",
+    remotePatterns: [
+      {
+        protocol: "https" ,
+        hostname: process.env.DOMAIN! ,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
