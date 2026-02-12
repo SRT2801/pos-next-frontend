65c5c5d01255cbec903ddf109a2f99510df82833
SRT DEV <sahirruiz4@gmail.com>
2026-02-11 23:46:20 -0500

    feat: Refactor schemas for better readability and add ShoppingCartSchema

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,23 +1,35 @@
-import {z} from "zod";
+import { z } from "zod";
 
 export const ProductSchema = z.object({
-    id: z.number(),
-    name: z.string(),
-    image: z.string(),
-    price: z.coerce.number(),
-    inventory: z.number(),
-    categoryId: z.number()
-})
+  id: z.number(),
+  name: z.string(),
+  image: z.string(),
+  price: z.coerce.number(),
+  inventory: z.number(),
+  categoryId: z.number(),
+});
 
 export const CategorySchema = z.object({
-    id: z.number(),
-    name: z.string()
-})
+  id: z.number(),
+  name: z.string(),
+});
 
 export const CategoriesResponseSchema = z.array(CategorySchema);
 
 export const CategoryWithProductsResponseSchema = CategorySchema.extend({
-    products: z.array(ProductSchema)
+  products: z.array(ProductSchema),
 });
 
+const ShoppingCartContentsSchema = ProductSchema.pick({
+  name: true,
+  image: true,
+  price: true,
+  inventory: true,
+}).extend({
+    productId: z.number(),
+    quantity: z.number(),
+});
+
+export const ShoppingCartSchema = z.array(ShoppingCartContentsSchema);
 export type Product = z.infer<typeof ProductSchema>;
+export type ShoppingCart = z.infer<typeof ShoppingCartSchema>;
