57f06f46657510f9172d06fec37b8195911c006c
SRT DEV <sahirruiz4@gmail.com>
2026-02-15 12:55:31 -0500

    fix: Add discount display logic to Amount component

--- components/Cart/Amount.tsx
+++ components/Cart/Amount.tsx
@@ -3,18 +3,19 @@ import { formatCurrency } from "@/src/utils";
 type AmountProps = {
     label: string;
     amount: number;
+    discount?: boolean;
 
 }
 
-export default function Amount({ label, amount }: AmountProps) {
+export default function Amount({ label, amount, discount }: AmountProps) {
   return (
-    <div className="flex justify-between">
+    <div className={`${discount && "line-through text-red-500"} flex justify-between`}>
         <dt className="font-bold">
             {label}
         </dt>
 
         <dd className="text-gray-900">
-            {formatCurrency(amount)}
+            {discount && <span className="line-through text-red-500">Discount: </span>}{formatCurrency(amount)}
         </dd>
     </div>
   )
