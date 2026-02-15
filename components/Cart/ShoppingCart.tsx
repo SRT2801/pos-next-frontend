07a6886f6ecc61353cbce8e0db4234511c521380
SRT DEV <sahirruiz4@gmail.com>
2026-02-15 13:03:08 -0500

    fix: Add discount display and CouponForm to ShoppingCart component

--- components/Cart/ShoppingCart.tsx
+++ components/Cart/ShoppingCart.tsx
@@ -3,19 +3,20 @@
 import { useStore } from "@/src/store"
 import ShoppingCartItem from "./ShoppingCartItem"
 import Amount from "./Amount"
+import CouponForm from "./CouponForm"
 
 
 export default function ShoppingCart() {
 
   const contents = useStore((state) => state.contents)
   const total = useStore((state) => state.total)
+  const discount = useStore((state) => state.discount)
 
   return (
     <>
 
       {contents.length ? (
         <>
-
           <h2 className='text-4xl font-bold text-gray-900'> Resumen de Venta</h2>
 
           <ul role="list" className="mt-6 divide-y divide-gray-200 border-gray-200 text-sm font-medium text-gray-500" >
@@ -29,10 +30,16 @@ export default function ShoppingCart() {
           </ul>
 
           <dl className="space-y-6 border-t border-gray-300 py-6 text-sm font-medium text-gray-500">
+            {discount ? ( 
+              <Amount label="Descuento" amount={discount} discount={true} />
+            ) : null}
+
             <Amount label="Total" amount={total} />
-
-
           </dl>
+          <CouponForm>
+
+          </CouponForm>
+
         </>
 
       ) : (
