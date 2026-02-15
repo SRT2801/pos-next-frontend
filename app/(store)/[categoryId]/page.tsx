e03279888348e33bebc0aee387ba53e2d729b0f5
SRT DEV <sahirruiz4@gmail.com>
2026-02-15 14:42:18 -0500

    fix: Add fetch options for caching products by category

--- app/(store)/[categoryId]/page.tsx
+++ app/(store)/[categoryId]/page.tsx
@@ -6,7 +6,11 @@ type Params = Promise<{ categoryId: string }>
 
 async function getProducts(categoryId: string) {
     const url = `${process.env.API_URL}/categories/${categoryId}?products=true`;
-    const req = await fetch(url)
+    const req = await fetch(url, {
+        next: {
+            tags: ["products-by-category"],
+        }
+    })
     const json = await req.json();
     if (!req.ok) {
         redirect('/1')
@@ -19,7 +23,7 @@ export default async function StorePage({ params }: { params: Params }) {
     const { categoryId } = await params;
 
     const category = await getProducts(categoryId);
-    
+
 
     return (
         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
