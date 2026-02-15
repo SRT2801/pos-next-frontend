e865f8729bbdcf2d17606cfdd4cd1c7d22ad33ef
SRT DEV <sahirruiz4@gmail.com>
2026-02-15 14:43:08 -0500

    refactor: Improve formatting and structure of schemas in schemas.ts

--- src/schemas.ts
+++ src/schemas.ts
@@ -26,15 +26,38 @@ const ShoppingCartContentsSchema = ProductSchema.pick({
   price: true,
   inventory: true,
 }).extend({
-    productId: z.number(),
-    quantity: z.number(),
+  productId: z.number(),
+  quantity: z.number(),
 });
 
 export const ShoppingCartSchema = z.array(ShoppingCartContentsSchema);
 export const CouponResponseSchema = z.object({
-    name: z.string().default(""),
-    message: z.string(),
-    percentage: z.coerce.number().min(0).max(100).default(0),
+  name: z.string().default(""),
+  message: z.string(),
+  percentage: z.coerce.number().min(0).max(100).default(0),
+});
+
+const OrderContentSchema = z.object({
+  productId: z.number(),
+  quantity: z.number(),
+  price: z.number(),
+});
+export const OrderSchema = z.object({
+  total: z.number(),
+  coupon: z.string(),
+  contents: z
+    .array(OrderContentSchema)
+    .min(1, { message: "El Carrito no puede ir vacio" }),
+});
+
+
+export const SuccessResponseSchema = z.object({
+  message: z.string(),
+});
+export const ErrorResponseSchema = z.object({
+  message: z.array(z.string()),
+  error: z.string(),
+  statusCode: z.number(),
 });
 export type Product = z.infer<typeof ProductSchema>;
 export type ShoppingCart = z.infer<typeof ShoppingCartSchema>;
