4e5bfeff3a8ce0c707d54992bcc8ab2bf8cf431d
SRT DEV <sahirruiz4@gmail.com>
2026-02-15 14:42:24 -0500

    fix: Refactor Amount component for improved readability

--- components/Cart/Amount.tsx
+++ components/Cart/Amount.tsx
@@ -8,15 +8,15 @@ type AmountProps = {
 }
 
 export default function Amount({ label, amount, discount }: AmountProps) {
-  return (
-    <div className={`${discount && "line-through text-red-500"} flex justify-between`}>
-        <dt className="font-bold">
-            {label}
-        </dt>
+    return (
+        <div className={`${discount && "line-through text-red-500"} flex justify-between`}>
+            <dt className="font-bold">
+                {label}
+            </dt>
 
-        <dd className="text-gray-900">
-            {discount && <span className="line-through text-red-500">Discount: </span>}{formatCurrency(amount)}
-        </dd>
-    </div>
-  )
+            <dd className="text-gray-900">
+                {discount && <span className="line-through text-red-500">Discount: </span>}{formatCurrency(amount)}
+            </dd>
+        </div>
+    )
 }
