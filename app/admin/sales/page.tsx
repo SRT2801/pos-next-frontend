9dbb7a09cdc74dcfaaf7fa2534364084dac43ece
SRT DEV <sahirruiz4@gmail.com>
2026-02-16 11:44:32 -0500

    feat: Implement Sales page with transactions filtering and data fetching

--- app/admin/sales/page.tsx
+++ app/admin/sales/page.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import TransactionsFilter from "@/components/transactions/TransactionsFilter";
+import Headings from "@/components/ui/Headings";
+import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { format } from "date-fns/format";
+import { getSalesByDate } from "@/src/api";
 
-export default function SalesPage() {
+export default async function SalesPage() {
+
+  const queryClient = new QueryClient();
+  const today = new Date();
+  const formattedDate = format(today, 'yyyy-MM-dd');
+
+  await queryClient.prefetchQuery({
+    queryKey: ['sales', formattedDate],
+    queryFn: () => getSalesByDate(formattedDate)
+  })
+
+
   return (
-    <div>SalesPage</div>
+    <>
+      <Headings>Sales</Headings>
+      <p className="text-lg">This is the Sales page.</p>
+
+      <HydrationBoundary state={dehydrate(queryClient)}>
+
+        <TransactionsFilter />
+
+      </HydrationBoundary>
+
+    </>
   )
 }
-    
