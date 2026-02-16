5094db2a2427bc8410ac8bc699659c7212debd4f
SRT DEV <sahirruiz4@gmail.com>
2026-02-16 11:45:01 -0500

    feat: Define schemas for transaction responses and contents

--- src/schemas.ts
+++ src/schemas.ts
@@ -50,7 +50,6 @@ export const OrderSchema = z.object({
     .min(1, { message: "El Carrito no puede ir vacio" }),
 });
 
-
 export const SuccessResponseSchema = z.object({
   message: z.string(),
 });
@@ -59,7 +58,25 @@ export const ErrorResponseSchema = z.object({
   error: z.string(),
   statusCode: z.number(),
 });
+
+export const ContentsSchema = z.object({
+  id: z.number(),
+  quantity: z.number(),
+  price: z.string(),
+  product: ProductSchema,
+});
+export const TransactionResponseSchema = z.object({
+  id: z.number(),
+  total: z.string(),
+  transactionDate: z.string(),
+  discount: z.string(),
+  coupon: z.string().nullable(),
+  contents: z.array(ContentsSchema),
+});
+
+export const TransactionsResponseSchema = z.array(TransactionResponseSchema);
 export type Product = z.infer<typeof ProductSchema>;
 export type ShoppingCart = z.infer<typeof ShoppingCartSchema>;
 export type CartItem = z.infer<typeof ShoppingCartContentsSchema>;
 export type Coupon = z.infer<typeof CouponResponseSchema>;
+export type Transaction = z.infer<typeof TransactionResponseSchema>;
