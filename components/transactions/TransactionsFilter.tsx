725f0b5fcef20d3f018eebf1a7455623141c49cd
SRT DEV <sahirruiz4@gmail.com>
2026-02-16 12:05:35 -0500

    feat: Add total calculation and display in TransactionsFilter component

--- components/transactions/TransactionsFilter.tsx
+++ components/transactions/TransactionsFilter.tsx
@@ -1,12 +1,14 @@
 "use client"
 
-import { use, useState } from "react";
+import { useState } from "react";
 import Calendar from "react-calendar"
 import 'react-calendar/dist/Calendar.css';
 import { format } from "date-fns";
 import { getSalesByDate } from "@/src/api";
 import { useQuery } from "@tanstack/react-query";
 import TransactionSummary from "./TransactionsSummary";
+import { formatCurrency } from "@/src/utils";
+
 
 type valuePiece = Date | null
 type value = valuePiece | [valuePiece, valuePiece]
@@ -20,9 +22,11 @@ export default function TransactionsFilter() {
         queryFn: () => getSalesByDate(formattedDate)
     })
 
+    const total = data?.reduce((total, transaction) => total + +transaction.total, 0) || 0;
+
     return (
-        <div className=" grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10">
-            <div>
+        <div className=" grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10 relative items-start">
+            <div className="lg:sticky lg:top-10">
                 <Calendar
                     value={date}
                     onChange={setDate}
@@ -31,12 +35,19 @@ export default function TransactionsFilter() {
 
             <div>
                 {isLoading && <p className="text-lg text-center">Loading transactions...</p>}
-                { data?  data.length ? data.map((transaction) => (
+                {data ? data.length ? data.map((transaction) => (
                     <TransactionSummary
                         key={transaction.id}
                         transaction={transaction}
                     />
                 )) : <p className="text-lg text-center">No transactions found for this date.</p> : null}
+
+                <p className="my-5 text-lg font-bold text-right" > total del dia: {''}
+                    <span className="font-normal">
+                         {formatCurrency(total)}
+                    </span>
+
+                </p>
             </div>
 
         </div>
