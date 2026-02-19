e8e92facfaff52af0e2667d5ce5d8fb52f797170
SRT DEV <sahirruiz4@gmail.com>
2026-02-19 16:36:41 -0500

    feat: Create a NotFound component to handle products not found

---
+++ app/admin/products/[id]/not-found.tsx
@@ -0,0 +1,13 @@
+import Heading from "@/components/ui/Headings";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="text-center">
+        <Heading>Producto no encontrado</Heading>
+
+        <p>Tal vez quieras volver a <Link className="text-green-400" href="/admin/products?page=1">Productos</Link></p>
+
+    </div>
+  )
+}
