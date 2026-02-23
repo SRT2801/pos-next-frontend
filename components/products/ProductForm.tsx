479e2accd8c52ae14125478c5b599a88b80b87fb
SRT DEV <sahirruiz4@gmail.com>
2026-02-22 20:51:27 -0500

    feat: Add UploadProductImage component to ProductForm for image handling

--- components/products/ProductForm.tsx
+++ components/products/ProductForm.tsx
@@ -1,4 +1,5 @@
 import { CategoriesResponseSchema, Product } from "@/src/schemas";
+import UploadProductImage from "./UploadProductImage";
 
 async function getCategories() {
     const url = `${process.env.API_URL}/categories`;
@@ -81,6 +82,11 @@ export default async function ProductForm({product}: {product?: Product}) {
 
                 </select>
             </div>
+
+
+            <UploadProductImage
+                currentImage={product?.image}
+            />
 
         </>
     )
