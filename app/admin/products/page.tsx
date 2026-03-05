5cb9c11edd8f654c84fee5e286c91be3f0cca3a7
SRT DEV <sahirruiz4@gmail.com>
2026-03-05 12:44:01 -0500

    feat: Implement pagination in ProductsPage component for improved product navigation

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,23 +1,46 @@
 import { serverApiFetch } from "@/services/serverApi";
 import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Headings";
+import Pagination from "@/components/ui/Pagination";
 import { ProductsResponseSchema } from "@/src/schemas";
+import { isValidPage } from "@/src/utils";
+import { redirect } from "next/navigation";
 
-async function getProducts() {
-    const json = await serverApiFetch(`/products`);
+async function getProducts(take: number, skip: number) {
+    const json = await serverApiFetch(`/products?take=${take}&skip=${skip}`);
     const data = ProductsResponseSchema.parse(json);
-    return data.products;
+    return {
+        products: data.products,
+        total: data.total
+    }
 }
 
-export default async function ProductsPage() {
+type SearchParams = Promise<{ page: string }>
 
-    const products = await getProducts();
+export default async function ProductsPage({ searchParams }: { searchParams: SearchParams }) {
+
+    const page = await searchParams
+    if (!isValidPage(+page.page)) redirect("/admin/products?page=1");
+
+    const productsPerPage = 10;
+    const skip = (+page.page - 1) * productsPerPage;
+
+    const { products, total } = await getProducts(productsPerPage, skip);
+
+    const totalPages = Math.ceil(total / productsPerPage);
+    if (+page.page > totalPages) redirect(`/admin/products?page=1`);
 
     return (
         <>
             <Heading>Administración de productos</Heading>
 
             <ProductsTable products={products} />
+
+            <Pagination
+                page={+page.page}
+                totalPages={totalPages}
+                basePath="/admin/products"
+            />
         </>
     )
 }
