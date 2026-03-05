396357cd550dd7a63e2c56cd8e30ef526beba2c6
SRT DEV <sahirruiz4@gmail.com>
2026-03-05 12:36:44 -0500

    refactor: Simplify ProductsPage component by removing pagination logic and adjusting product fetching

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,51 +1,23 @@
 import { serverApiFetch } from "@/services/serverApi";
 import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Headings";
-import Pagination from "@/components/ui/Pagination";
 import { ProductsResponseSchema } from "@/src/schemas";
-import { isValidPage } from "@/src/utils";
-import Link from "next/link";
-import { redirect } from "next/navigation";
 
-async function getProducts(take: number, skip: number) {
-    const json = await serverApiFetch(`/products?take=${take}&skip=${skip}`);
+async function getProducts() {
+    const json = await serverApiFetch(`/products`);
     const data = ProductsResponseSchema.parse(json);
-    return {
-        products: data.products,
-        total: data.total
-    }
+    return data.products;
 }
 
-type SearchParams = Promise<{ page: string }>
+export default async function ProductsPage() {
 
-export default async function ProductsPage({ searchParams }: { searchParams: SearchParams }) {
-
-    const page = await searchParams
-    if (!isValidPage(+page.page)) redirect("/admin/products?page=1");
-
-    const productsPerPage = 10;
-    const skip = (+page.page - 1) * productsPerPage;
-
-    const { products, total } = await getProducts(productsPerPage, skip);
-
-    const totalPages = Math.ceil(total / productsPerPage);
-    if (+page.page > totalPages) redirect(`/admin/products?page=1`);
+    const products = await getProducts();
 
     return (
         <>
-            <Link href="/admin/products/new"
-                className="rounded-lg bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-6 sm:px-10 text-sm sm:text-base transition-colors inline-block">Crear producto</Link>
-
             <Heading>Administración de productos</Heading>
 
             <ProductsTable products={products} />
-
-            <Pagination
-                page={+page.page}
-                totalPages={totalPages}
-                basePath="/admin/products"
-            />
-
         </>
     )
 }
